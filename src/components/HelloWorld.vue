63f2650af17b1acf99cf3583b960391d89dd7e14
Terceros <diego.terceros@ikhon.com.br>
2022-12-10 17:51:00 -0400

    atualizacao

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,103 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+  <div class="div_principal">
+    <v-container>
+      <v-row>
+        <h1 class="central">Conversor de moedas</h1>
+      </v-row>
+      <v-row>
+        <v-col
+          cols="2">
+          <form action="post">
+            <v-text-field
+            label="Valor"
+            hide-details="auto"
+            type="number"
+            min="0"
+          ></v-text-field>
+          </form>
+        </v-col>
+        <v-col
+        cols="3">
+          <v-select
+          :items="symbols"
+          label="De"
+          outlined
+          v-model="de"
+        ></v-select>
+        </v-col>
+        <v-col
+        cols="3">
+          <v-select
+          :items="symbols"
+          label="Para"
+          outlined
+          v-model="para"
+        ></v-select>
+        </v-col>
+      </v-row>
+      <v-row>
+        <v-btn elevation="2" color="primary">
+          Converter
+        </v-btn>
+      </v-row>
+    </v-container>
   </div>
 </template>
 
 <script>
+
+const parseXML = require('xml-parse-from-string')
+
 export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
+  data(){
+    return{
+      symbols: [],
+      de : null,
+      para: null
+    }
+  },
+  methods : {
+    async getSymbols(){
+      var myHeaders = new Headers();
+
+      var requestOptions = {
+        method: 'GET',
+        redirect: 'follow',
+        headers: myHeaders
+      };
+
+      return fetch(`http://localhost:3000/`, requestOptions)
+        .then(response => 
+          this.symbols = parseXML(response)
+        )
+        .catch(error => console.log('error', error));
+    },
+
+    async convert(from, to, amount){
+      var myHeaders = new Headers();
+      myHeaders.append("apikey", "Q0ENl8BQdQNLhtQN4aAHrsjfv9cuPevu");
+
+      var requestOptions = {
+        method: 'GET',
+        redirect: 'follow',
+        headers: myHeaders
+      };
+
+      return fetch(`https://api.apilayer.com/exchangerates_data/convert?to={${to}}&from={${from}}&amount={${amount}}`, requestOptions)
+        .then(response => response.text())
+        .then(result => console.log(result))
+        .catch(error => console.log('error', error));
+    }
+  },
+  async mounted(){
+    await this.getSymbols()
   }
 }
+
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
+<style>
+  .central{
+    text-align: center;
+  }
 </style>
